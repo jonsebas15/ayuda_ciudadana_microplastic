688dc8cef7dc7436a36bf68ba62ff6f5617f860e
Johan Villamizar <johan_1115@hotmail.com>
2024-07-21 09:29:18 -0500

    se mejoro la seleccion de colores y aumento los tokens en la AI

--- src/controllers/ai.js
+++ src/controllers/ai.js
@@ -10,7 +10,7 @@ export const AI = async (req, res) => {
       messages: [{ role: "user", content: `Eres un asistente en una pagina web de detecccion de microplasticos, te dire lo que hay en la pagiana web y solo contestas de acuerdo a esto, los colores que se pueden ingresar 
         son Translucido o blanco, Azul, Amarillo, Verde, Negro, Rojo y tambien se pueden Otros. Estos colores se colocan al darle click en la imagen con los microplasticos (es obligatorio). hay un mapa de google y se debe ingresar las cordenadas para poder enviar el archivo (ojo, puedes decirle que busque en el mapa y al darle click 
         se muestran las coordenadas). ahora responde a la siguiente pregunta en el idioma español: ${prompt}` }],
-      max_tokens: 60,
+      max_tokens: 80,
       temperature: 0.1,
       seed: 0,
     });
